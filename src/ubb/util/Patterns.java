70651d088ace66c659c7cf350f89af547e633e80
Iulian <Iulian@DESKTOP-CG3TAU6>
2017-05-23 00:45:03 +0300

    Solved bugs for foreign key.

--- src/ubb/util/Patterns.java
+++ src/ubb/util/Patterns.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public enum Patterns {
 	CREATE_DB("^CREATE DATABASE [A-Z_a-z]+$"),
 	DROP_DB("^DROP DATABASE [A-Z_a-z]+$"),
-	CREATE_TABLE("^CREATE TABLE [A-Z_a-z]+ +\\([A-Z_a-z0-9]+ *(?:CHAR\\([1-9]+[0-9]*\\)|NUMBER)+( NOT NULL)?(?: PRIMARY KEY| UNIQUE| FOREIGN KEY ON [A-Za-z0-9]+\\.[A-Za-z0-9]+)?( *, *[A-Z_a-z0-9]+ (?:CHAR\\([1-9]+[0-9]*\\)|NUMBER)( NOT NULL)?(?: PRIMARY KEY| UNIQUE| FOREIGN KEY ON [A-Za-z0-9]+\\.[A-Za-z0-9]+)?)*\\)$"),
+	CREATE_TABLE("^CREATE TABLE [A-Z_a-z]+ +\\([A-Z_a-z0-9]+ *(?:CHAR\\([1-9]+[0-9]*\\)|NUMBER)+( NOT NULL)?(?: PRIMARY KEY| UNIQUE| FOREIGN KEY ON [A-Z_a-z0-9]+\\.[A-Z_a-z0-9]+)?( *, *[A-Z_a-z0-9]+ (?:CHAR\\([1-9]+[0-9]*\\)|NUMBER)( NOT NULL)?(?: PRIMARY KEY| UNIQUE| FOREIGN KEY ON [A-Za-z_0-9]+\\.[A-Za-z0-9]+)?)*\\)$"),
 	DROP_TABLE("^DROP TABLE [A-Z_a-z]+.[A-Z_a-z]+$"),
 	SET_SCHEMA("^SET SCHEMA [A-Z_a-z0-9]+$"),
 	CREATE_INDEX("^CREATE *(UNIQUE)? INDEX [A-Z_a-z]+ ON [A-Z_a-z]+ +\\( *[A-Z_a-z]+ *(, *[A-Z_a-z]+ *)*\\)$"),
